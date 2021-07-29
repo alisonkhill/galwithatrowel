5d6c97a3fb27812c885e6bcdd90058b3f4e25237
Alison Hill <alison.hill@aya.yale.edu>
2021-07-29 19:51:30 -0400

    Trying a new array function for JavaScript

---
+++ array-func-alt.js
@@ -0,0 +1,26 @@
+// This is an alternate way to use an array to display a random image in the header of the page.
+
+// Array of various photos from the Gallery.
+
+let random_images_array = [
+    'foxglove.jpg',
+    'Facebook cover, backyard photos.jpg',
+    'Mimosas.jpg',
+    'peonies-bloom.jpg',
+    'peony-coral.jpg',
+    'phlox-creeping.jpg',
+    'pink-dangly.jpg'
+];
+
+// Function to display a random image from the array. I followed a tutorial found here: http://www.dyn-web.com/code/random-image-js/
+
+function getRandomImage(imgAr, path) {
+    path = 'images/';
+    let num = Math.floor(Math.random() * imgAr.length);
+    let img = imgAr[num];
+    let imgStr = '<img src ="' + path + img + '"alt = "">';
+    document.write(imgStr); document.close();
+}
+
+// Calling the function
+getRandomImage(random_images_array);
--- array-func.js
+++ array-func.js
@@ -1,25 +1,22 @@
-// Array and function to display an image at random in the header
-// Array of various photos from the Gallery.
+//Setting the function to run when page loads
 
+window.onload = displayImage;
+
+// Array of various photos from the Gallery.
 let random_images_array = [
-    'foxglove.jpg',
-    'Facebook cover, backyard photos.jpg',
-    'Mimosas.jpg',
-    'peonies-bloom.jpg',
-    'peony-coral.jpg',
-    'phlox-creeping.jpg',
-    'pink-dangly.jpg'
+    'images/foxglove.jpg',
+    'images/Facebook cover, backyard photos.jpg',
+    'images/Mimosas.jpg',
+    'images/peonies-bloom.jpg',
+    'images/peony-coral.jpg',
+    'images/phlox-creeping.jpg',
+    'images/pink-dangly.jpg'
 ];
 
-// Function to display a random image from the array. I followed a tutorial found here: http://www.dyn-web.com/code/random-image-js/
+// Function to display a random image from the array using Math functions and array.length calculation
 
-function getRandomImage(imgAr, path) {
-    path = 'images/';
-    let num = Math.floor(Math.random() * imgAr.length);
-    let img = imgAr[num];
-    let imgStr = '<img src ="' + path + img + '"alt = "">';
-    document.write(imgStr); document.close();
+function displayImage () {
+    let randomNumber = Math.floor(Math.random() * random_images_array.length);
+    document.getElementById("headerImage").src = random_images_array[randomNumber];
 }
 
-// Calling the function
-getRandomImage(random_images_array);
